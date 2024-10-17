bec8ced1c56b26499b1931aa877a85d56ab9decf
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-10-17 22:27:37 +0600

    features data fetching done

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -1,11 +1,23 @@
-
+import { useEffect, useState } from "react";
 
 const Features = () => {
-    return (
-        <div>
-            <h1>our Features</h1>
-        </div>
-    );
+  const [features, setFeatures] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("/public/features.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setFeatures(data?.features);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Our Features : {features.length}</h1>
+    </div>
+  );
 };
 
 export default Features;
