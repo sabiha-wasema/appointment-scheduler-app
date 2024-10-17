b490c6aeb4ceea24a40583034114e641752b78e2
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-10-17 21:59:07 +0600

    folder structure and footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-slate-500  text-white shadow-lg py-7 mt-20">
+        <div className="container mx-auto text-center">
+          <p>
+            <span>&copy; 2024</span> Giga-Tech Limited. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-white shadow-lg">
+      <nav className="bg-slate-500 text-white shadow-lg">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <div className="text-2xl font-bold text-gray-700">
-            Doctor's Appointment 
+          <div className="text-2xl font-bold text-white">
+          Giga-Tech Limited 
           </div>
           <div className="space-x-4">
-            <Link to="/" className="text-gray-700 hover:text-blue-600">
+            <Link to="/" className="text-white hover:text-gray-300">
               Home
             </Link>
-            <Link to="/login" className="text-gray-700 hover:text-blue-600">
+            <Link to="/login" className="text-white hover:text-gray-300">
               Login
             </Link>
-            <Link to="/register" className="text-gray-700 hover:text-blue-600">
+            <Link to="/register" className="text-white hover:text-gray-300">
               Register
             </Link>
           </div>
