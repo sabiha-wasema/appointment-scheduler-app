7d173698d476b7c3c93e7459da5acadaa0c8203e
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-10-18 16:20:10 +0600

    app components responsive donr

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ManageAppointments from "./components/Manage/ManageAppointments";
 import ScheduleAppointment from "./components/Schedule/SheduleAppointment";
 import UserSearch from "./components/Users/UserSearch";
 import { ToastContainer } from 'react-toastify';
+
 function App() {
   const [selectedUserId, setSelectedUserId] = useState(null);
 
@@ -11,14 +12,17 @@ function App() {
   };
   
   return (
-   <div className="flex flex-col justify-center items-center">
-    
-    <h1 className="text-3xl mt-10 font-bold">Appointment Scheduler</h1>
-    <ToastContainer />
-      <UserSearch onUserSelect={handleUserSelect} />
-      {selectedUserId && <ScheduleAppointment targetUserId={selectedUserId} />}
-      <ManageAppointments />
-   </div>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-green-500 to-teal-500 p-4">
+      <h1 className="text-3xl mt-10 font-bold text-center">Appointment Scheduler</h1>
+      <ToastContainer />
+      <div className="w-full max-w-md mt-5">
+        <UserSearch onUserSelect={handleUserSelect} />
+        {selectedUserId && <ScheduleAppointment targetUserId={selectedUserId} />}
+      </div>
+      <div className="w-full max-w-4xl mt-8">
+        <ManageAppointments />
+      </div>
+    </div>
   );
 }
 
